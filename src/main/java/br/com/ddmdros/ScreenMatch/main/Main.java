f4c2334033aced9a45ef915ed8913b744ad34f8c
ddmdros <diogome_deiros@hotmail.com>
2025-09-10 18:29:27 -0300

    practing streams

--- src/main/java/br/com/ddmdros/ScreenMatch/main/Main.java
+++ src/main/java/br/com/ddmdros/ScreenMatch/main/Main.java
@@ -1,14 +1,16 @@
 package br.com.ddmdros.ScreenMatch.main;
 
 import br.com.ddmdros.ScreenMatch.model.EpisodeData;
+import br.com.ddmdros.ScreenMatch.model.Episode;
 import br.com.ddmdros.ScreenMatch.model.SeasonData;
 import br.com.ddmdros.ScreenMatch.model.SeriesData;
 import br.com.ddmdros.ScreenMatch.service.ApiRequest;
 import br.com.ddmdros.ScreenMatch.service.DataConverter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -29,15 +31,50 @@ public class Main {
         SeriesData data = converter.getData(json,SeriesData.class);
         System.out.println(data);
 
-		for (int i = 1; i <= data.totalSeasons(); i++){
-			json = apiRequest.getData
+        for (int i = 1; i <= data.totalSeasons(); i++){
+            json = apiRequest.getData
                     (OMDB_WEB_ADDRESS + serieName.replace(" ", "+") + "&season=" + i + API_KEY);
-			SeasonData seasonData = converter.getData(json, SeasonData.class);
-			seasons.add(seasonData);
-		}
-		seasons.forEach(System.out::println);
+            SeasonData seasonData = converter.getData(json, SeasonData.class);
+            seasons.add(seasonData);
+        }
+        seasons.forEach(System.out::println);
 
         seasons.forEach(s -> s.episodes().forEach(t -> System.out.println(t.title())));
+
+        List<EpisodeData> episodesData = seasons.stream()
+                .flatMap(t -> t.episodes()
+                        .stream()).collect(Collectors.toList());
+
+        System.out.println("Top 5 episodes");
+        episodesData.stream()
+                .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(EpisodeData::rating).reversed())
+                .map(e -> e.title().toUpperCase())
+                .limit(10)
+                .forEach(System.out::println);
+
+        List <Episode> episodes = seasons.stream()
+                .flatMap(s -> s.episodes().stream()
+                        .map(e -> new Episode(s.season(), e)))
+                .collect(Collectors.toUnmodifiableList());
+
+        episodes.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        var year = scanner.nextInt();
+        scanner.nextLine();
+
+        LocalDate searchDate = LocalDate.of(year,1,1);
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodes.stream()
+                .filter(e -> e.getReleaseDate() != null
+                        && e.getReleaseDate().isAfter(searchDate)).forEach(e -> System.out.println(
+                                "Season: " + e.getSeason()
+                                        + " Episode: " + e.getTitle()
+                                        + " Release Date: " + e.getReleaseDate().format(df)
+                ));
 
     }
 
